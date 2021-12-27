c1500cc9f723f48fb8ea06f41da40e40576a2a8e
kiseki <986223884@qq.com>
2021-12-27 17:09:29 +0800

    update

--- node.go
+++ node.go
@@ -1,6 +1,13 @@
 package dolt
 
+import (
+	"bytes"
+	"fmt"
+	"sort"
+)
+
 type node struct {
+	bucket     *Bucket
 	isLeaf     bool
 	unbalanced bool
 	spilled    bool
@@ -32,4 +39,44 @@ func (n *node) minKeys() int {
 		return 1
 	}
 	return 2
+}
+
+func (n *node) size() int {
+	sz, elsz := pageHeaderSize, n.pageElementSize()
+	for i := 0; i < len(n.inodes); i++ {
+		item := &n.inodes[i]
+		sz += elsz + len(item.key) + len(item.value)
+	}
+	return sz
+}
+
+func (n *node) sizeLessThan(v int) bool {
+	sz, elsz := pageHeaderSize, n.pageElementSize()
+	for i := 0; i < len(n.inodes); i++ {
+		item := &n.inodes[i]
+		sz += elsz + len(item.key) + len(item.value)
+		if sz >= v {
+			return false
+		}
+	}
+	return true
+}
+
+func (n *node) pageElementSize() int {
+	if n.isLeaf {
+		return leafPageElementSize
+	}
+	return branchPageElementSize
+}
+
+func (n *node) childAt(index int) *node {
+	if n.isLeaf {
+		panic(fmt.Sprintf("invalid childAt(%d) on a leaf node", index))
+	}
+	return n.bucket.node(n.inodes[index].pgid, n)
+}
+
+func (n *node) childIndex(child *node) int {
+	index := sort.Search(len(n.inodes), func(i int) bool { return bytes.Compare(n.inodes[i], key, child.key) != -1 })
+	return index
 }
